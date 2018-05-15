39f238e36a1416222375396e4fe797c491f77eb7
SonodaYomi <625522391@qq.com>
2018-05-15 22:17:18 +0800

    卡片互动实装
    做了吃东西的相关动作和互动，目前还没有连接上控制逻辑

--- UnityUI/Assets/ControllScripts/FindTarget.cs
+++ UnityUI/Assets/ControllScripts/FindTarget.cs
@@ -7,11 +7,17 @@ public class FindTarget : MonoBehaviour {
 
     public GameObject target;
     public GameObject character;
-    public GameObject text;
+    public GameObject text,text2,text3;
+
+    private CharacterController chaCon;
+    private Animator ani;
 
 	// Use this for initialization
 	void Start () {
-        character = this.gameObject;	
+        character = this.gameObject;
+        ani = this.transform.GetComponent<Animator>();
+        ani.SetBool("foodPosition", false);
+        chaCon = this.transform.GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
@@ -20,10 +26,37 @@ public class FindTarget : MonoBehaviour {
         {
             text.GetComponent<TextMesh>().text = "Found";
             character.transform.LookAt(target.transform);
+            if (ani.GetBool("foodPosition")==false)
+            chaCon.Move(this.transform.forward * 70.0f * Time.deltaTime);
+            ani.SetBool("foodTargetOn", true);
         }
         else
         {
             text.GetComponent<TextMesh>().text = "Offline";
+            ani.SetBool("foodTargetOn", false);
+        }
+
+        text2.GetComponent<TextMesh>().text = "Target:"+ ani.GetBool("foodTargetOn").ToString();
+        text3.GetComponent<TextMesh>().text = "Position:"+ ani.GetBool("foodPosition").ToString();
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Cheese")
+        {
+            
+            ani.SetBool("foodPosition", true);
+
+        }
+       
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.name == "LeaveCheese")
+        {
+            ani.SetBool("foodPosition", false);
         }
     }
 }
--- UnityUI/Assets/ControllScripts/SceneController.cs
+++ UnityUI/Assets/ControllScripts/SceneController.cs
@@ -6,8 +6,10 @@ public class SceneController : MonoBehaviour {
 
     static public bool characterOn;
     static public bool foodOn;
+    
 	// Use this for initialization
 	void Start () {
+        
         characterOn = false;
         foodOn = false;
 	}
