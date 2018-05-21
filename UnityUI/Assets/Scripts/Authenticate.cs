b5868056b1abf99179db094540fe375f24d15303
sgh <154050727@qq.com>
2018-05-22 00:18:57 +0800

    登录与序列号验证的网络连接

--- UnityUI/Assets/Scripts/Authenticate.cs
+++ UnityUI/Assets/Scripts/Authenticate.cs
@@ -1,21 +1,34 @@
 ﻿using FairyGUI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Runtime.InteropServices;
 
 public class Authenticate : MonoBehaviour
 {
     private GComponent mainUI;
     // Use this for initialization
+    [DllImport("testDLL")]
+    private static extern int ser(byte[] a, byte[] b);
     void Start()
     {
         mainUI = GetComponent<UIPanel>().ui;
         GButton okButton = mainUI.GetChild("n25").asButton;
         GButton resetButton = mainUI.GetChild("n26").asButton;
-
+        GTextField cdkey = mainUI.GetChild("n8").asTextField;
 
         okButton.onClick.Set((EventContext) =>
         {
-            SceneManager.LoadSceneAsync("mainScene");
+            string cdkey1 = cdkey.text;
+            
+            byte[] cdkey2 = System.Text.Encoding.Default.GetBytes(cdkey1);
+            
+
+            int judge = ser(cdkey2, cdkey2);
+            if (judge == 0)
+            {
+                SceneManager.LoadSceneAsync("mainScene");
+            }
+            
         });
 
 
--- UnityUI/Assets/Scripts/SignToMain.cs
+++ UnityUI/Assets/Scripts/SignToMain.cs
@@ -1,19 +1,37 @@
 ﻿using FairyGUI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Runtime.InteropServices;
 
 public class SignToMain : MonoBehaviour
 {
     private GComponent mainUI;
     // Use this for initialization
+
+    [DllImport("testDLL")]
+    private static extern int Login(byte[] a, byte[] b, byte[] c);
+
     void Start()
     {
         mainUI = GetComponent<UIPanel>().ui;
         GButton login = mainUI.GetChild("n15").asButton;
         GButton register = mainUI.GetChild("n14").asButton;
+        GTextField id = mainUI.GetChild("n8").asTextField;
+        GTextField pass = mainUI.GetChild("n9").asTextField;
+
 
         login.onClick.Set((EventContext) => {
-            SceneManager.LoadSceneAsync("MainScene");
+            string id1 = id.text;
+            string pass1 = pass.text;
+            byte[] id2 = System.Text.Encoding.Default.GetBytes(id1);
+            byte[] pass2 = System.Text.Encoding.Default.GetBytes(pass1);
+        
+            int judge = Login(id2, pass2, pass2);
+            if (judge == 0)
+            {
+                SceneManager.LoadSceneAsync("MainScene");
+            }
+            
         });
         register.onClick.Set((EventContext) => {
            
